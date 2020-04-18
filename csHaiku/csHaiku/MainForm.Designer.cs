2492c858d2630f3ded56dee67416b1e83d8cb319
bobby45800 <bobby45900@gmail.com>
2020-04-18 14:00:03 +0100

    VS to Github
    Full code at current state

---
+++ csHaiku/csHaiku/MainForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Joshua
+ * Date: 08/10/2018
+ * Time: 10:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace csHaiku
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtNoun1 = new System.Windows.Forms.TextBox();
+			this.txtNoun2 = new System.Windows.Forms.TextBox();
+			this.txtVerb = new System.Windows.Forms.TextBox();
+			this.txtAdjective = new System.Windows.Forms.TextBox();
+			this.btnSubmit = new System.Windows.Forms.Button();
+			this.btnExample = new System.Windows.Forms.Button();
+			this.lblOutput1 = new System.Windows.Forms.Label();
+			this.lblOutput2 = new System.Windows.Forms.Label();
+			this.lblOutput3 = new System.Windows.Forms.Label();
+			this.btnGenerateEx2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 35);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Noun";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 71);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Noun";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(14, 140);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Verb";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(13, 105);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Adjective";
+			// 
+			// txtNoun1
+			// 
+			this.txtNoun1.Location = new System.Drawing.Point(120, 35);
+			this.txtNoun1.Name = "txtNoun1";
+			this.txtNoun1.Size = new System.Drawing.Size(100, 20);
+			this.txtNoun1.TabIndex = 4;
+			// 
+			// txtNoun2
+			// 
+			this.txtNoun2.Location = new System.Drawing.Point(120, 68);
+			this.txtNoun2.Name = "txtNoun2";
+			this.txtNoun2.Size = new System.Drawing.Size(100, 20);
+			this.txtNoun2.TabIndex = 5;
+			// 
+			// txtVerb
+			// 
+			this.txtVerb.Location = new System.Drawing.Point(120, 137);
+			this.txtVerb.Name = "txtVerb";
+			this.txtVerb.Size = new System.Drawing.Size(100, 20);
+			this.txtVerb.TabIndex = 6;
+			// 
+			// txtAdjective
+			// 
+			this.txtAdjective.Location = new System.Drawing.Point(120, 102);
+			this.txtAdjective.Name = "txtAdjective";
+			this.txtAdjective.Size = new System.Drawing.Size(100, 20);
+			this.txtAdjective.TabIndex = 7;
+			// 
+			// btnSubmit
+			// 
+			this.btnSubmit.Location = new System.Drawing.Point(12, 186);
+			this.btnSubmit.Name = "btnSubmit";
+			this.btnSubmit.Size = new System.Drawing.Size(77, 23);
+			this.btnSubmit.TabIndex = 8;
+			this.btnSubmit.Text = "Write!";
+			this.btnSubmit.UseVisualStyleBackColor = true;
+			this.btnSubmit.Click += new System.EventHandler(this.BtnSubmitClick);
+			// 
+			// btnExample
+			// 
+			this.btnExample.Location = new System.Drawing.Point(95, 186);
+			this.btnExample.Name = "btnExample";
+			this.btnExample.Size = new System.Drawing.Size(85, 23);
+			this.btnExample.TabIndex = 10;
+			this.btnExample.Text = "Generate Ex";
+			this.btnExample.UseVisualStyleBackColor = true;
+			this.btnExample.Click += new System.EventHandler(this.BtnExampleClick);
+			// 
+			// lblOutput1
+			// 
+			this.lblOutput1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblOutput1.Location = new System.Drawing.Point(13, 222);
+			this.lblOutput1.Name = "lblOutput1";
+			this.lblOutput1.Size = new System.Drawing.Size(596, 35);
+			this.lblOutput1.TabIndex = 11;
+			// 
+			// lblOutput2
+			// 
+			this.lblOutput2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblOutput2.Location = new System.Drawing.Point(13, 257);
+			this.lblOutput2.Name = "lblOutput2";
+			this.lblOutput2.Size = new System.Drawing.Size(596, 35);
+			this.lblOutput2.TabIndex = 12;
+			// 
+			// lblOutput3
+			// 
+			this.lblOutput3.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblOutput3.Location = new System.Drawing.Point(13, 292);
+			this.lblOutput3.Name = "lblOutput3";
+			this.lblOutput3.Size = new System.Drawing.Size(596, 35);
+			this.lblOutput3.TabIndex = 13;
+			// 
+			// btnGenerateEx2
+			// 
+			this.btnGenerateEx2.Location = new System.Drawing.Point(186, 186);
+			this.btnGenerateEx2.Name = "btnGenerateEx2";
+			this.btnGenerateEx2.Size = new System.Drawing.Size(86, 23);
+			this.btnGenerateEx2.TabIndex = 14;
+			this.btnGenerateEx2.Text = "Generate Ex2";
+			this.btnGenerateEx2.UseVisualStyleBackColor = true;
+			this.btnGenerateEx2.Click += new System.EventHandler(this.BtnGenerateEx2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(289, 379);
+			this.Controls.Add(this.btnGenerateEx2);
+			this.Controls.Add(this.lblOutput3);
+			this.Controls.Add(this.lblOutput2);
+			this.Controls.Add(this.lblOutput1);
+			this.Controls.Add(this.btnExample);
+			this.Controls.Add(this.btnSubmit);
+			this.Controls.Add(this.txtAdjective);
+			this.Controls.Add(this.txtVerb);
+			this.Controls.Add(this.txtNoun2);
+			this.Controls.Add(this.txtNoun1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "csHaiku";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button btnGenerateEx2;
+		private System.Windows.Forms.Label lblOutput3;
+		private System.Windows.Forms.Label lblOutput2;
+		private System.Windows.Forms.Label lblOutput1;
+		private System.Windows.Forms.Button btnExample;
+		private System.Windows.Forms.Button btnSubmit;
+		private System.Windows.Forms.TextBox txtAdjective;
+		private System.Windows.Forms.TextBox txtVerb;
+		private System.Windows.Forms.TextBox txtNoun2;
+		private System.Windows.Forms.TextBox txtNoun1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ csHaiku/csHaiku/MainForm.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace csHaiku {
+	public partial class MainForm : Form {
+		public MainForm() {
+			InitializeComponent();
+		}
+
+        #region variables
+
+       	//Initialising variables
+        string nounInput1 = "";
+		string nounInput2 = "";
+		string adjectiveInput = "";
+		string verbInput = "";
+		
+		string[] inputtedWords = new string[5];
+		int[] numVowels = new int[5];
+
+		List<string[]> fillWordsList = new List<string[]> {
+	    	new string[10] {"I", "you", "we", "they", "she", "he", "time", "God", "happines", "sadnes"}, //Pronoun
+			new string[8] {"hate", "coldness", "heart", "faith", "skin", "flight", "grave", "world"}, //Noun
+			new string[8] {"water", "earth", "soil", "clouds", "emptiness", "space", "cavern", "shadows"}, //Place
+			new string[8] {"in", "into", "between", "through", "past", "across", "near", "beneath"}, //Preposition
+			new string[8] {"a", "the", "my", "our", "his", "her", "their", "your"}, //Article
+			new string[8] {"slither", "crawl", "creep", "dive", "stroll", "push", "drift", "paddle"}, //verbAction
+			new string[8] {"gaze", "listen", "stare", "ignore", "weep", "shudder", "fall", "smile"}, //verbNoAction
+			new string[8] {"cold", "wet", "slow", "torpid", "silent", "harsh", "misty", "senseless"}, //adjective
+		};
+		
+		string line1 = "";
+		string line2 = "";
+		string line3 = "";
+		
+		Random rng = new Random();
+		int randomNum = 0;
+
+        #endregion
+
+       	void inputChecks() {
+            /// <summary>
+            /// When a text field is left empty, 
+            /// The variables are just set to a random from the existing arrays
+            /// Set the right array element to equal the outcome of the pre-existing words
+            /// Else,
+            /// Set the right array element to equal the outcome of the inputs
+	        /// </summary>
+	            
+	        inputtedWords[1] = nounInput1;
+	        inputtedWords[2] = nounInput2;
+	        inputtedWords[3] = adjectiveInput;
+	        inputtedWords[4] = verbInput;
+	           
+            if (nounInput1 == "") {
+				randomNum = rng.Next(0, 8);
+				inputtedWords[1] = fillWordsList[2][randomNum];
+            }
+			if (nounInput2 == "") {
+				randomNum = rng.Next(0, 8);
+				inputtedWords[2] = fillWordsList[2][randomNum];
+			}
+			if (adjectiveInput == "") {
+				randomNum = rng.Next(0, 8);
+				inputtedWords[3] = fillWordsList[7][randomNum];
+			}
+			if (verbInput ==  "") {
+				randomNum = rng.Next(0, 8);
+				inputtedWords[4] = fillWordsList[6][randomNum];
+			}
+			
+			syllableCounter();
+		}
+		
+        void syllableCounter() {
+	        /// <summary>
+	        /// First, the program loops through the algorithm for each word inputted, as shows in the inputtedWords array
+	       	/// Then each character is checked through in currentword, 
+	        /// 	-Each vowel is checked through
+	        /// 	-If the current char is not a vowel, do not do anything (other than changing some bools for the algorithm)
+	        /// 	-If the current char is a vowel, add 1 to the numberofvowels found
+	        /// 	-If a vowel has not been found, set lastWasVowel to false so that the algorithm can continue to repeat
+	        /// 	-Just to remove some silent "es" and "e"s, some checking from the end of the word is done so that the vowel "e" can be removed as it is counted as silent
+	        /// </summary>
+	        for (int i = 1; i < 5; i++) {
+	        	char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+	            numVowels[i] = 0;
+	        	bool lastWasVowel = false;
+	        		
+	        	foreach (char wc in inputtedWords[i]) {
+	        		bool foundVowel = false;
+	        		foreach (char v in vowels) {
+	        			if (v == wc && lastWasVowel) {
+	        				foundVowel = true;
+	        				lastWasVowel = true;
+	        				break;
+	        			} else if (v == wc && !lastWasVowel) {
+	        				numVowels[i]++;
+	        				foundVowel = true;
+	        				lastWasVowel = true;
+	        				break;
+	        			}
+	        		}
+	        		if (!foundVowel) {
+	        			lastWasVowel = false;
+	        		}
+	        	}
+	        	
+	        	//Removing possibility of silent "es"
+	        	if (inputtedWords[i].Length > 2 && inputtedWords[i].Substring(inputtedWords[i].Length - 2) == "es") {
+	        		numVowels[i]--;
+	        	//Removing possibility of silent "e"
+	        	} else if (inputtedWords[i].Length > 1 && inputtedWords[i].Substring(inputtedWords[i].Length - 1) == "e") {
+	        		numVowels[i]--;
+	        	}
+	        	
+	        	switch (i) {
+	        		case 1:
+	        			if (numVowels[i] > 2) {
+	        				inputtedWords[i] = fillWordsList[1][randomNum];
+	        			}
+	        			break;
+	        		case 2:
+		                if (numVowels[i] > 2) {
+		                   	inputtedWords[i] = fillWordsList[1][randomNum];
+		                }
+	                    break;
+	                case 3:
+		                if (numVowels[i] > 3) {
+		                    inputtedWords[i] = fillWordsList[7][randomNum];
+		                }
+	                    break;
+	                 case 4:
+		                if (numVowels[i] > 4) {
+		                    inputtedWords[i] = fillWordsList[5][randomNum];
+		                }
+	                    break;
+	        	}
+	        }
+	    }
+        
+		void grammarChecks() {
+	        /// <summary>
+	        /// If the first noun's last character is an "s",
+	        /// Add an "s" to the verb
+	        /// Else,
+	        ///     If the verb already has an "s" on its end,
+	        ///     Set its end to "ed" because the noun doesn't have "s" at the end
+	        ///     Else,
+	        ///     Still add an "s" onto the end of the verb
+	        /// </summary>
+	        char[] trimChar = {'s'};
+	        if (inputtedWords[1].Substring(inputtedWords[1].Length -1, 1) == "s") {
+	        	if (inputtedWords[4].Substring(inputtedWords[4].Length -1, 1) == "s") {
+	        		inputtedWords[4] = inputtedWords[4] + "";
+	        	} else {
+	        		inputtedWords[4] = inputtedWords[4] + "s";
+	        	}
+            	
+			} else {
+				if (inputtedWords[4].Substring(inputtedWords[4].Length -1, 1) == "s") {
+					inputtedWords[4] = inputtedWords[4].TrimEnd(trimChar) + "ed";
+				} else {
+					inputtedWords[4] = inputtedWords[4] + "s";
+				}
+			}
+		}
+
+        void line1Build() {
+	        /// <summary>
+	        /// The first line is simple 
+	        ///     -Add the randomised words from existing arrays to each other
+	        /// </summary>
+			randomNum = rng.Next(0, 8);
+			line1 = fillWordsList[4][randomNum] + " ";
+			
+			randomNum = rng.Next(0, 8);
+			line1 = line1 + fillWordsList[7][randomNum] + " " + inputtedWords[1] + " " + inputtedWords[4];
+		}
+
+        void line2Build() {
+        	/// <summary>
+       		/// The second line is slightly more complex
+       	 	///     -The first word is a pre-existing preposition
+       	 	///     -The second word is an pre-existing fillWordsList[5]
+       	 	///     -Third word is the inputted adjective
+       	 	///     -Final word is the inputted noun
+       		/// </summary>
+			randomNum = rng.Next(0, 8);
+			line2 = fillWordsList[3][randomNum];
+			
+			randomNum = rng.Next(0, 8);
+			line2 = line2 + " " + fillWordsList[4][randomNum] + " " + inputtedWords[3] + " " + inputtedWords[2] + ", ";
+		}
+        
+        void line3Build() {
+	        /// <summary>
+	        /// Third line is FAR more complex
+	        ///     If the pre-existing ajective (randomly generated) ends with a "y"
+	        ///     Add an "ily" to the end of it
+	        ///     Else,
+	        ///     Add an "ly"
+	        ///     
+	        ///     Ranomising the . and , for the second word end
+	        ///		     
+	        ///     Adding a pre-existing pronoun to the third line
+	        ///     If one of the pronouns is in the second subcatergory
+	        /// 	Add an "s" onto the end
+	  	    /// 
+	   	    /// 	If one of the pre-existing verbs is in the second subcatergory
+	   	    /// 	Add it to the line
+	   	   	/// 	Else,
+	   	    /// 	If there is an "s" on the end of the word
+	   	    /// 		Add the word to the line
+	   	    /// 		Else
+	   	    /// 		If there is no "s" on the end of the word
+	   	    /// 		Add an "s" onto the end, and then add it
+	   	    /// 		
+	   		/// </summary>
+	   		
+	        //FIRST WORD
+			randomNum = rng.Next(0, 8);
+			if (fillWordsList[7][randomNum].Substring(fillWordsList[7][randomNum].Length -1, 1) == "y") {
+				char[] trimChars = {'y'};
+				line3 = (fillWordsList[7][randomNum].TrimEnd(trimChars) + "ily");
+			} else {
+				line3 = (fillWordsList[7][randomNum].TrimEnd() + "ly");
+			}
+			
+			//COMMA OR FULL STOP SWITCHER
+			if (rng.Next(0, 3) > 1) {
+				line3 = line3 + ".";
+			} else {
+				line3 = line3 + ",";
+			}
+			
+			//SECOND WORD
+			randomNum = rng.Next(0, 10);
+			line3 = line3 + " " + fillWordsList[0][randomNum];
+			if (randomNum > 6) {
+				line3 = line3 + "s";
+			}
+			
+			//THIRD WORD
+			randomNum = rng.Next(0, 8);
+			if (randomNum < 4) {
+				//TODO: Find a way to incoportate if the pronoun is in second subcategory (aka God+), make sure the verbnoaction does have an "s" added to it, if not then dont
+				line3 = line3 + " " + fillWordsList[6][randomNum];
+			} else {
+				if (fillWordsList[6][randomNum].Substring(fillWordsList[6][randomNum].Length - 1) == "s") {
+					MessageBox.Show("Last word does have s at the end");
+					line3 = line3 + " " + fillWordsList[6][randomNum];
+				} else {
+					char[] trimChar2 = {'s'};
+					line3 = line3 + " " + fillWordsList[6][randomNum].TrimEnd(trimChar2);
+				}
+			}
+        }
+		
+		void BtnSubmitClick(object sender, EventArgs e) {
+			line1 = "";
+			line2 = "";
+			line3 = "";
+			
+			nounInput1 = txtNoun1.Text;
+			nounInput2 = txtNoun2.Text;
+			adjectiveInput = txtAdjective.Text;
+			verbInput = txtVerb.Text;
+		
+			inputChecks();
+			grammarChecks();
+			line1Build();
+			line2Build();
+			line3Build();
+			
+			lblOutput1.Text = line1;
+			lblOutput2.Text = line2;
+			lblOutput3.Text = line3;
+		}
+		
+		void BtnExampleClick(object sender, EventArgs e) {
+			txtNoun1.Text = "table";
+			txtNoun2.Text = "chair";
+			txtVerb.Text = "sit";
+			txtAdjective.Text = "big";
+		}
+		
+		void BtnGenerateEx2Click(object sender, EventArgs e) {
+			txtNoun1.Text = "shmeememus";
+			txtNoun2.Text = "regulator";
+			txtVerb.Text = "superduperyla";
+			txtAdjective.Text = "enourmous";			
+		}
+	}
+}
